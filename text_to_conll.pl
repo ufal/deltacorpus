22a615bab08859d045864f567c6e3d1c3bd783ef
Dan Zeman <zeman@ufal.mff.cuni.cz>
2018-02-06 17:19:33 +0100

    forgotten commit two years ago?

--- text_to_conll.pl
+++ text_to_conll.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl
+#!/usr/bin/env perl
 
 use strict;
 use warnings;
@@ -10,9 +10,13 @@ binmode STDOUT, ":utf8";
 my $MAX_TOKENS = 1000000;
 my $token_cnt = 0;
 
-while (<>) {
+while (<>)
+{
     chomp;
     my $line = $_;
+    # Tentatively split the line on existing whitespace only.
+    # We will later compare this with the tokenization that takes punctuation into account, and save the points where there were no spaces.
+    my @pretokens = split(/\s+/, $line);
     # tokenize
     $line =~ s/(\w+)/ $1 /g;
     $line =~ s/([^\s\w]+)/ $1 /g;
@@ -21,9 +25,41 @@ while (<>) {
     $line =~ s/^\s+//g;
     $line =~ s/\s+$//g;
     my @tokens = split /\s/, $line;
-    foreach my $i (0 .. $#tokens) {
+    # Compare tokens to pretokens. Identify places where we inserted a space.
+    my @nospaceafter;
+    if(scalar(@tokens) != scalar(@pretokens))
+    {
+        if(scalar(@tokens) < scalar(@pretokens))
+        {
+            die('Merger of tokens was not expected');
+        }
+        my $j = 0;
+        for(my $i = 0; $i<=$#tokens;)
+        {
+            if($tokens[$i] eq $pretokens[$j])
+            {
+                $nospaceafter[$i] = 0;
+                $i++;
+                $j++;
+            }
+            # If token is not identical to pretoken then token should be a prefix of pretoken.
+            elsif($pretokens[$j] =~ s/^\Q$tokens[$i]\E(.+)$/$1/)
+            {
+                $nospaceafter[$i] = 1;
+                $i++;
+            }
+            # We are not supposed to ever come here.
+            else
+            {
+                die("Unexpected pretoken '$pretokens[$j]' and token '$tokens[$i]'");
+            }
+        }
+    }
+    foreach my $i (0 .. $#tokens)
+    {
         my $ord = $i + 1;
-        print "$ord\t$tokens[$i]\t_\t_\t_\t_\t_\n";
+        my $misc = $nospaceafter[$i] ? 'SpaceAfter=No' : '_';
+        print "$ord\t$tokens[$i]\t_\t_\t_\t_\t_\t_\t_\t$misc\n";
         $token_cnt++;
     }
     print "\n";
